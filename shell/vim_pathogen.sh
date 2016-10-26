a5d73afc93293c77bb8dce682d011755bb6c4096
hacker65536 <s.hacker65536@gmail.com>
2016-10-26 08:45:23 +0000

    add shell of vim

---
+++ shell/vim_pathogen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+mkdir -p ~/.vim/{autoload,bundle} && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
---
+++ shell/vim_solarized.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir ~/.vim/colors
+curl -o ~/.vim/colors/solarized.vim https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
